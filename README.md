1c629a9a56f997f8c426cfb4d06eaac36e5d8cce
desktop <psomail@yandex.ru>
2023-05-21 20:40:55 +0300

    task1 ready

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# tasks_learn_sql
+# The repository is for learning SQL
+|№|**Task**|**Description**|
+|--|--|--|
+|1|**[Task1](https://github.com/iamseryy/tasks_learn_oop/tree/main/task1)**|Установка СУБД, подключение к БД, просмотр и создание таблиц|
--- task1/task1.sql
+++ task1/task1.sql
@@ -1,12 +1,49 @@
-CREATE SCHEMA IF NOT EXISTS `task1`;
+DROP DATABASE IF EXISTS task_1;
+CREATE DATABASE task_1;
+USE task_1;
 
-USE task1;
-
+/*
+	1. Создайте таблицу с мобильными телефонами. 
+    Заполните БД данными (поля и наполнение см. в презентации)
+*/
 CREATE TABLE mobile_phones
 (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(20) NOT NULL,
     manufacturer VARCHAR(20) NOT NULL,
     product_count INT,
     price INT 
 );
+
+INSERT INTO mobile_phones (product_name, manufacturer, product_count, price)
+VALUES 
+('iPhone X', 'Apple', 3, 76000),
+('iPhone 8', 'Apple', 2, 51000),
+('Galaxy S9', 'Samsung', 2, 56000),
+('Galaxy S8', 'Samsung', 1, 41000),	
+('P20 Pro', 'Huawei', 5, 36000);
+
+-- 2. Выведите название, производителя и цену для товаров, количество которых превышает 2
+SELECT manufacturer, price 
+FROM mobile_phones
+WHERE product_count > 2;
+
+-- 3. Выведите весь ассортимент товаров марки “Samsung”
+SELECT id, product_name, manufacturer, product_count, price 
+FROM mobile_phones
+WHERE manufacturer = 'Samsung';
+
+-- 4.  С помощью регулярных выражений найти:
+-- 4.1. Товары, в которых есть упоминание "Iphone"
+SELECT id, product_name, manufacturer, product_count, price 
+FROM mobile_phones
+WHERE product_name like '%Iphone%'
+	OR manufacturer like '%Iphone%'; 
+
+-- 4.2. Товары, в которых есть упоминание"Samsung"
+SELECT id, product_name, manufacturer, product_count, price 
+FROM mobile_phones
+WHERE product_name like '%Samsung%'
+	OR manufacturer like '%Samsung%'; 
+
+-- 4.3. Товары, в которых есть ЦИФРЫ
