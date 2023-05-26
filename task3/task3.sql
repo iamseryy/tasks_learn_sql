493b149d18bedf5c0a18142e7b5a72e279bfc596
desktop <psomail@yandex.ru>
2023-05-26 22:20:39 +0300

    task3 ready

--- task3/task3.sql
+++ task3/task3.sql
@@ -37,3 +37,83 @@ VALUES
 1. Отсортируйте данные по полю заработная плата (salary) в порядке: убывания; возрастания 
 */
 
+-- в порядке убывания
+SELECT firstname, lastname, post, seniority, salary, age
+FROM staff
+ORDER BY salary DESC;
+
+-- в порядке возрастания
+SELECT firstname, lastname, post, seniority, salary, age
+FROM staff
+ORDER BY salary;
+
+/*
+2. Выведите 5 максимальных заработных плат (salary)
+*/
+
+SELECT DISTINCT salary
+FROM staff
+ORDER BY salary DESC
+LIMIT 5;
+
+/*
+3. Посчитайте суммарную зарплату (salary) по каждой специальности (роst)
+*/
+
+SELECT 
+	post, 
+    SUM(salary) AS total_salary
+FROM staff
+GROUP BY post;
+
+/*
+4. Найдите кол-во сотрудников с специальностью (post) «Рабочий» в возрасте от 24 до 49 лет включительно.
+*/
+
+SELECT COUNT(id) AS staff_count
+FROM staff
+WHERE 
+	post = 'Рабочий'
+    and age BETWEEN 24 AND 49;
+
+/*
+5. Найдите количество специальностей
+*/
+
+SELECT COUNT(DISTINCT post) AS post_count
+FROM staff;
+
+/*
+6. Выведите специальности, у которых средний возраст сотрудников меньше 30 лет 
+*/
+
+SELECT 
+	post,
+    AVG(age) AS avg_age
+FROM staff
+GROUP BY post
+HAVING avg_age < 30;
+
+-- или если только вывести одно поле post
+-- вариант 1
+SELECT post
+FROM 
+	(
+		SELECT 
+			staff_sub.post AS post,
+			AVG(staff_sub.age) AS avg_age
+		FROM staff AS staff_sub
+		GROUP BY post
+		HAVING avg_age < 30
+	) AS post_list;
+    
+-- вариант 2
+SELECT DISTINCT staff_main.post
+FROM staff AS staff_main
+WHERE EXISTS ( 
+				SELECT AVG(staff_sub.age) AS avg_age
+				FROM staff AS staff_sub
+				WHERE staff_sub.post = staff_main.post
+                HAVING avg_age < 30
+            );
+
