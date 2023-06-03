f699b52fa66afd7b450a803cccb7793e5f4e9da2
desktop <psomail@yandex.ru>
2023-06-03 12:25:39 +0300

    task4.4 ready

--- task4/task4.sql
+++ task4/task4.sql
@@ -49,6 +49,48 @@ SELECT
 SELECT CONCAT(users.firstname, ' ', users.lastname) AS 'Users who did not send messages'
 FROM users
 LEFT JOIN messages ON users.id = messages.from_user_id
-WHERE messages.from_user_id is NULL
-;
+WHERE messages.from_user_id is NULL;
+
+
+-- 4. Пусть задан некоторый пользователь. Из всех друзей этого пользователя найдите человека, который больше всех написал ему сообщений.
+SET @user := 1;
+
+SELECT CONCAT(users.firstname, ' ', users.lastname) AS 'best friend'
+FROM (
+	SELECT 	
+	COUNT(messages.id) AS count_mess, 
+	messages.from_user_id AS id
+	FROM messages
+	WHERE 
+		messages.to_user_id = @user
+		AND messages.from_user_id IN (
+										SELECT initiator_user_id AS id FROM friend_requests 
+										WHERE target_user_id = @user AND status = 'approved'
+										UNION
+										SELECT target_user_id FROM friend_requests 
+										WHERE initiator_user_id = @user AND status = 'approved'
+									)
+	GROUP BY messages.from_user_id
+	HAVING count_mess = (
+		SELECT MAX(count_mess)
+		FROM (
+			SELECT 	
+					COUNT(messages.id) AS count_mess, 
+					messages.from_user_id as friend
+					FROM messages
+					WHERE 
+						messages.to_user_id = @user
+						AND messages.from_user_id IN (
+														SELECT initiator_user_id AS id FROM friend_requests 
+														WHERE target_user_id = @user AND status ='approved'
+														UNION
+														SELECT target_user_id FROM friend_requests 
+														WHERE initiator_user_id = @user AND status ='approved'
+													)
+					GROUP BY messages.from_user_id
+		) AS friend_count_mess
+	)
+) AS best_friend
+JOIN users ON best_friend.id = users.id;
+                            
 
