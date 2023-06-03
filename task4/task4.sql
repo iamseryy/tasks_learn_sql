7e9a8a669287b2c5cc89f989920f35bdc0ec9514
desktop <psomail@yandex.ru>
2023-06-03 09:09:00 +0300

    task4.1 ready

--- task4/task4.sql
+++ task4/task4.sql
@@ -1,12 +1,23 @@
--- Подсчитать общее количество лайков, которые получили пользователи младше 12 лет.
 USE task_4;
 
-SELECT COUNT(likes.id) AS likes
+-- 1. Подсчитать общее количество лайков, которые получили пользователи младше 12 лет.
+SELECT COUNT(likes.id) AS 'Total number of likes received by users under 12'
 FROM likes
 WHERE 
-	likes.user_id IN (
-			SELECT profiles.user_id
-            FROM profiles
-            WHERE TIMESTAMPDIFF(YEAR, profiles.birthday, NOW()) < 12
-    );
+	likes.media_id IN (	
+		SELECT 	media.id
+		FROM media
+		JOIN profiles ON media.user_id = profiles.user_id
+		WHERE TIMESTAMPDIFF(YEAR, profiles.birthday, NOW()) < 12
+	);
+
+    
+    
+    
+    
+-- 2.Определить кто больше поставил лайков (всего): мужчины или женщины. 
+
+
+
+    
 
