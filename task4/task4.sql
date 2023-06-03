2540478f64f015cbc62ff243d58b97001988d75e
desktop <psomail@yandex.ru>
2023-06-03 10:58:36 +0300

    task4.3 ready

--- task4/task4.sql
+++ task4/task4.sql
@@ -1,7 +1,8 @@
 USE task_4;
 
+
 -- 1. Подсчитать общее количество лайков, которые получили пользователи младше 12 лет.
-SELECT COUNT(likes.id) AS 'Total number of likes received by users under 12'
+SELECT COUNT(likes.id) AS 'Total likes received by users under 12'
 FROM likes
 WHERE 
 	likes.media_id IN (	
@@ -11,7 +12,8 @@ WHERE
 		WHERE TIMESTAMPDIFF(YEAR, profiles.birthday, NOW()) < 12
 	);
     
--- 2.Определить кто больше поставил лайков (всего): мужчины или женщины. 
+    
+-- 2. Определить кто больше поставил лайков (всего): мужчины или женщины. 
 SELECT 
 	(
 		CASE 
@@ -40,4 +42,13 @@ SELECT
         
 			ELSE 'woman'
         
-	END) AS 'Who liked more: man or woman'
+	END) AS 'Who liked more: man or woman';
+
+
+-- 3. Вывести всех пользователей, которые не отправляли сообщения.
+SELECT CONCAT(users.firstname, ' ', users.lastname) AS 'Users who did not send messages'
+FROM users
+LEFT JOIN messages ON users.id = messages.from_user_id
+WHERE messages.from_user_id is NULL
+;
+
