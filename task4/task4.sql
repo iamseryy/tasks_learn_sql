6cb2b67b634aa5b2b1e4e0bc33f718041b0b389d
desktop <psomail@yandex.ru>
2023-06-03 10:07:31 +0300

    task4.2 ready

--- task4/task4.sql
+++ task4/task4.sql
@@ -1,5 +1,5 @@
 USE task_4;
-
+/*
 -- 1. Подсчитать общее количество лайков, которые получили пользователи младше 12 лет.
 SELECT COUNT(likes.id) AS 'Total number of likes received by users under 12'
 FROM likes
@@ -10,14 +10,33 @@ WHERE
 		JOIN profiles ON media.user_id = profiles.user_id
 		WHERE TIMESTAMPDIFF(YEAR, profiles.birthday, NOW()) < 12
 	);
-
-    
-    
-    
-    
+    */
 -- 2.Определить кто больше поставил лайков (всего): мужчины или женщины. 
-
-
-
-    
-
+SELECT 
+	(CASE 
+			WHEN (NOT EXISTS(SELECT likes.id FROM likes)) THEN 'no likes yet'
+    		WHEN (
+					(	SELECT COUNT(likes.id) 
+						FROM likes
+						JOIN profiles ON likes.user_id = profiles.user_id
+						WHERE profiles.gender = 'm') = 
+													 (	SELECT COUNT(likes.id) 
+														FROM likes
+														JOIN profiles ON likes.user_id = profiles.user_id
+														WHERE profiles.gender = 'f')
+			) THEN 'equally'
+        
+			WHEN (
+					(	SELECT COUNT(likes.id) 
+						FROM likes
+						JOIN profiles ON likes.user_id = profiles.user_id
+						WHERE profiles.gender = 'm') >
+													 (	SELECT COUNT(likes.id) 
+														FROM likes
+														JOIN profiles ON likes.user_id = profiles.user_id
+														WHERE profiles.gender = 'f')
+			) THEN 'man'
+        
+			ELSE 'woman'
+        
+	END) AS 'Who liked more: man or woman'
