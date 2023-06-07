78ecbd20ce3897d4cf15d9987f532f6ca2274c29
Перминов Сергей Олегович <S_Perminov@bz.ru>
2023-06-07 10:53:45 +0300

    task 5 ready

--- task5/task5.sql
+++ task5/task5.sql
@@ -24,22 +24,32 @@ SELECT * FROM v_young_users;
 количеством сообщений) . (используйте DENSE_RANK)
 */
 
-
+-- вариант решения 1
 WITH message_rating AS
 ( 
 SELECT 
 		CONCAT(users.firstname, ' ', users.lastname) AS user,
-		COUNT(messages.id) AS total_messeges
+		COUNT(messages.id) AS total_messages
 	FROM users
 	LEFT JOIN messages ON users.id = messages.from_user_id 
 	GROUP BY user
-    ORDER BY total_messeges
+    ORDER BY total_messages
 )
 SELECT
 	user,
-    total_messeges,
-    DENSE_RANK() OVER(ORDER BY total_messeges DESC) AS rating
+    total_messages,
+    DENSE_RANK() OVER(ORDER BY total_messages DESC) AS rating
 FROM message_rating;
+
+-- вариант решения 2
+SELECT 
+		CONCAT(users.firstname, ' ', users.lastname) AS user,
+		COUNT(messages.id) AS total_messages,
+        DENSE_RANK() OVER(ORDER BY COUNT(messages.id) DESC) AS rating
+FROM users
+LEFT JOIN messages ON users.id = messages.from_user_id 
+GROUP BY user
+ORDER BY rating;
 
 
 /*
@@ -47,6 +57,8 @@ FROM message_rating;
 (created_at) и найдите разницу дат отправления между соседними сообщениями, 
 получившегося списка. (используйте LEAD или LAG)
 */
+
+-- вариант решения 1 - разница дат в днях, час., мин., сек.
 WITH sorted_messages AS
 ( 
 	SELECT
@@ -55,21 +67,31 @@ WITH sorted_messages AS
 		to_user_id,
 		body,
 		created_at,
-		LAG(created_at, 1, 0) OVER(ORDER BY created_at) AS prev_created_at,
-		LEAD(created_at, 1, 0) OVER(ORDER BY created_at) AS last_created_at
+        TIMESTAMPDIFF(SECOND, LAG(created_at, 1, 0) OVER(ORDER BY created_at), created_at) AS diff_in_sec
 	FROM messages
 	ORDER BY created_at
 )
 
+SELECT
+	id,
+	from_user_id,
+	to_user_id,
+	body,
+	created_at,
+	CONCAT(	  FLOOR(TIME_FORMAT(SEC_TO_TIME(diff_in_sec), '%H') / 24), ' days ',
+			  MOD(TIME_FORMAT(SEC_TO_TIME(diff_in_sec), '%H'), 24), 'h:',
+			  FLOOR(MOD(diff_in_sec, 3600) / 60), 'm:',
+			  MOD(diff_in_sec, 60), 's'
+			) AS difference
+FROM sorted_messages;
+
+-- вариант решения 2 - разница дат в час., мин., сек.
 SELECT
 		id,
 		from_user_id,
 		to_user_id,
 		body,
 		created_at,
-        TIMESTAMPDIFF(SECOND, prev_created_at, created_at) AS prev_diff,
-        TIMESTAMPDIFF(SECOND, created_at, last_created_at) AS last_diff
-FROM sorted_messages
-;
-
-
+        SEC_TO_TIME(TIMESTAMPDIFF(SECOND, LAG(created_at, 1, 0) OVER(ORDER BY created_at), created_at)) AS difference
+	FROM messages
+	ORDER BY created_at;
